7b9f0dfd2fb651bfddeac29880d83912fd4ee94e
Darwin <50497333+minotad66@users.noreply.github.com>
2020-01-06 17:11:20 -0500

    cambios

--- src/components/login/hotelList.js
+++ src/components/login/hotelList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, TextInput } from "react-native";
 import {
   Container,
   Header,
@@ -12,64 +12,65 @@ import {
   Icon,
   Left,
   H2,
-  Right,
-  Input
+  Right
 } from "native-base";
 import { FontAwesome } from "@expo/vector-icons";
 export default class CardImageExample extends Component {
   state = {
-    product: "",
+    product: [],
     text: "",
     array: []
   };
 
   componentDidMount() {
-    fetch("https://app.fakejson.com/q", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        token: "qn4JQJjcuUPfEfvLZq48Zw",
-        data: {
-          _repeat: 50,
-          id: "cryptoUUID",
-          name: "companyName",
-          image: "personAvatar",
-          start: "numberInt|0,5",
-          price: "numberInt|0,100000",
-          status: "productOrderStatus"
-        }
-      })
-    })
+    fetch("https://apihotel-v1-rest.herokuapp.com/hotels")
       .then(response => response.json())
-      .then(result => this.setState({ array: result }));
+      .then(result => this.setState({ array: result, product: result }));
   }
 
   login = () => {
-      this.props.navigation.navigate('Login')
-  }
+    this.props.navigation.navigate("Login");
+  };
+
+  filter = text => {
+    this.setState({ text });
+    let texto = this.state.text;
+    const data = this.state.array;
+    const newData = data.filter(item => {
+      const itemData = item.name.toUpperCase();
+      const textData = texto.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      product: this.state.text != '' ? newData : this.state.array
+    });
+    console.log(this.state.product);
+    
+  };
 
   render() {
     return (
       <Container>
         <Header searchBar rounded style={styles.header}>
-        <Left>
+          <Left>
             <Button transparent onPress={this.login}>
               <Icon name="arrow-back" />
             </Button>
           </Left>
           <Item>
             <Icon name="ios-search" />
-            <Input placeholder="Search" />
-            <Icon name="ios-people" />
+            <TextInput
+              value={this.state.text}
+              placeholder="Type here to translate!"
+              onChangeText={
+                text => this.filter(text)
+              }
+              value={this.state.text}
+            />
           </Item>
-          <Button>
-            <Text>Search</Text>
-          </Button>
         </Header>
         <Content>
-          {this.state.array.map(arr => (
+          {this.state.product.map(arr => (
             <Card>
               <CardItem cardBody>
                 <Image
@@ -82,14 +83,15 @@ export default class CardImageExample extends Component {
               </CardItem>
               <CardItem>
                 <Left>
-                  <Text>
+                  <Text style={styles.textColor}>Estrellas:</Text>
+                  <Text style={styles.textColor}>
                     {arr.start}{" "}
                     <FontAwesome name="star" size={15}></FontAwesome>{" "}
                   </Text>
                 </Left>
                 <Right>
                   <Text>Precio por noche</Text>
-                  <Text>ARS {arr.price}</Text>
+                  <Text style={styles.textColor}>ARS {arr.price}</Text>
                 </Right>
               </CardItem>
             </Card>
@@ -101,8 +103,11 @@ export default class CardImageExample extends Component {
 }
 
 const styles = StyleSheet.create({
-    header: {
-      marginTop: 20
-    }
-  });
-  
+  header: {
+    marginTop: 20
+  },
+  textColor: {
+    color: 'orange',
+    fontSize: 15
+  }
+});
